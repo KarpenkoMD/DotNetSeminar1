a1e891e6b456a4ee0273a7a81a2ad16d79ba884b
mikhailka <karpenko.md@gmail.com>
2022-04-26 22:50:48 +0300

    Second task completed

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a>=b) {
+    if (c > a)
+        Console.WriteLine("Самое большое число "+ c);
+    else
+        Console.WriteLine("Самое большое число "+ a); 
+}
+else {
+    if (c > b)
+        Console.WriteLine("Самое большое число "+ c);
+    else
+        Console.WriteLine("Самое большое число "+ b); 
+}
