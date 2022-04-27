48ce9be5a7fe79e10334340836b2724897fdfeb1
mikhailka <karpenko.md@gmail.com>
2022-04-27 14:44:51 +0300

    Task 3 started

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+do {
+Console.WriteLine("Для выходы нажмите q");
+Console.Write("Введите число: ");
+string s = Console.ReadLine();
+if (s =="q") break; 
+int a = Convert.ToInt32(s);
+
+if ( (a%2) != 0 ) 
+    Console.WriteLine("Число "+a+" четное");
+else 
+    Console.WriteLine("Число "+a+" нечетное");
+
+} while ( s !="q" );
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+do {
+Console.WriteLine("Для выходы нажмите q");
+Console.Write("Введите число: ");
+string s = Console.ReadLine();
+if (s =="q") break; 
+int N = Convert.ToInt32(s);
+
+if (N <= 1 ) {
+    Console.WriteLine("Вы ввели число меньше либо равно 1");
+    break; 
+}
+else {
+    Console.Write("Четные числа от 1 до "+N+": ");
+    for (int i = 2; i <= N; i++)
+         if ( (i%2) != 0 ) 
+             Console.Write(N+", ");
+}
+} while ( s !="q" );
