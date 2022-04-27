576aa800ac5b968dac4b640596569240801d2ef0
mikhailka <karpenko.md@gmail.com>
2022-04-27 19:50:50 +0300

    Task3 finished

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,15 +1,17 @@
 ﻿// на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-
+string s="q";
 do {
-Console.WriteLine("Для выходы нажмите q");
+Console.Clear();
+Console.WriteLine("Для выхода нажмите q");
 Console.Write("Введите число: ");
-string s = Console.ReadLine();
+s = Console.ReadLine();
 if (s =="q") break; 
 int a = Convert.ToInt32(s);
 
-if ( (a%2) != 0 ) 
+if ( (a%2) == 0 ) 
     Console.WriteLine("Число "+a+" четное");
 else 
     Console.WriteLine("Число "+a+" нечетное");
-
+    
+s = Console.ReadLine();
 } while ( s !="q" );
